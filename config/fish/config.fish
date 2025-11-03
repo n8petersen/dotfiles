358432de7f19817595f4a54c831b843a60e3aaf8
Nathan Petersen <n8knows@gmail.com>
2025-11-02 18:19:20 -0700

    add fish

---
+++ config/fish/config.fish
@@ -0,0 +1,28 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Disable Greeting
+set -U fish_greeting ""
+
+# Enable starship
+starship init fish | source
+
+# Enable brew
+# eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Configure env vars
+set SSH_AUTH_SOCK ~/.1password/agent.sock
+
+# Configure aliases
+alias clr "printf '\033[2J\033[3J\033[1;1H'"
+alias ll "ls -lah"
+alias kb kubectl
+alias nv='nvim'
+alias gst='git status'
+alias tf='terraform'
+alias kb='kubectl'
+alias kbc='kubectl config'
+alias ssh='kitty +kitten ssh'
+alias parupdate='paru -Syu --noconfirm'
+alias p='paru -Syu --noconfirm'

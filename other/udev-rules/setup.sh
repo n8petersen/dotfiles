e36b13bda6c251b691336a39ad0ea39147b88d6d
Nathan Petersen <n8knows@gmail.com>
2025-12-13 16:14:56 -0700

    fix udev script

--- other/udev-rules/setup.sh
+++ other/udev-rules/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if (( $EUID != 0 )); then
-    echo "This script must be run as root."
-    exit 1
+if (($EUID != 0)); then
+  echo "This script must be run as root."
+  exit 1
 fi
 
-echo "Creating '/dev/udev/rules.d' directory"
+echo "Creating '/etc/udev/rules.d' directory"
 sudo mkdir -p /etc/udev/rules.d
 
 echo "Copying touchscreen rules file to directory"
